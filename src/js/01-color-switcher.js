8f908d44e3cfd048464df46946fc628f398088d3
SvitlanaVelychko <svel2608@gmail.com>
2022-06-16 00:51:02 +0300

    Solve tasks

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,37 @@
+const DELAY = 1000; 
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.body,
+} 
+let colorSwitcherId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    colorSwitcherId = setInterval(() => {
+        const hexColor = getRandomHexColor();
+        refs.body.style.backgroundColor = hexColor;
+        colorSwitcherActive();
+    }, DELAY);
+}
+
+function onStopBtnClick() {
+    clearInterval(colorSwitcherId);
+    colorSwitcherStoped();
+}
+
+function colorSwitcherActive() {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+}
+
+function colorSwitcherStoped() {
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
